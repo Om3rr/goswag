d5ef9d747371e1f346559f51713c3f2527bb5021
Omer <Omertorrents@gmail.com>
2016-12-05 17:48:50 +0200

    push

--- mainApp.py
+++ mainApp.py
@@ -15,7 +15,7 @@ class Application(tornado.web.Application):
     def __init__(self):
         settings = {
             "debug": True,
-            "static_path": os.path.join(os.path.dirname(__file__), "static"),
+            "static_path": os.path.join(os.path.dirname(__file__)),
         }
         handlers = [
             (r"/", MainHandler),
